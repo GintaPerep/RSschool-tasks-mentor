c243cec7a523b310dd6c51932e8bf0c85e6af2c2
Ginta <ginta.aizstrauta@gmail.com>
2023-01-15 14:00:23 +0200

    feat: started keyboard part

---
+++ keyboard/src/Keyboard/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../utils/htmlToElement';
+import Keyboard from './keyboardBody.html';
+import './keyboardBody.scss';
+
+const keyboard = htmlToElement(Keyboard);
+
+export default keyboard;
--- keyboard/src/index.js
+++ keyboard/src/index.js
@@ -1,10 +1,12 @@
 import './styles.scss';
 import Header from './Header/index';
 import Textarea from './Textarea/index';
+import Keyboard from './Keyboard/index';
 
 const root = document.getElementById('root');
 root.append(Header);
 root.append(Textarea);
+root.append(Keyboard);
 
 /*
 class KeyboardButton {
